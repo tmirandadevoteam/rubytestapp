89c2b83aeb20f81504caee844b7cb808543ffa68
tmirandadevoteam <teresa.miranda@devoteam.com>
2022-07-20 16:55:24 +0100

    Add configs to the project - bootstrap not works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "controllers"
 import "bootstrap"
 
+//= require rails-ujs
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
+//= require activestorage
+//= require turbolinks
+//= require_tree .
